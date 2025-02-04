58666d1ae61ac19d5464328d5bddc79ef40c834a
Eli Fine <ejfine@gmail.com>
2025-02-04 20:09:04 +0000

    WIP

--- src/aws_central_infrastructure/iac_management/program.py
+++ src/aws_central_infrastructure/iac_management/program.py
@@ -1,5 +1,6 @@
 import logging
 
+import boto3
 from ephemeral_pulumi_deploy import get_aws_account_id
 from ephemeral_pulumi_deploy import get_config
 from ephemeral_pulumi_deploy import get_config_str
@@ -10,6 +11,9 @@ from pulumi_aws.iam import GetPolicyDocumentStatementPrincipalArgs
 from pulumi_aws.iam import get_policy_document
 from pulumi_aws.organizations import get_organization
 from pulumi_aws_native import s3
+
+from .shared_lib import WORKLOAD_INFO_SSM_PARAM_PREFIX
+from .shared_lib import AwsLogicalWorkload
 
 logger = logging.getLogger(__name__)
 
@@ -62,10 +66,61 @@ def pulumi_program() -> None:
     export("env", env)
 
     # Create Resources Here
-
+    organization_home_region = "us-east-1"
     central_state_bucket_name = get_config_str("proj:backend_bucket_name")
     _ = s3.BucketPolicy(
         "bucket-policy",
         bucket=central_state_bucket_name,
         policy_document=create_bucket_policy(central_state_bucket_name),
     )
+
+    ssm_client = boto3.client("ssm", region_name=organization_home_region)
+
+    parameters = []
+    next_token = None
+
+    while True:
+        # API call with optional pagination
+        response = ssm_client.describe_parameters(
+            ParameterFilters=[{"Key": "Name", "Option": "BeginsWith", "Values": [WORKLOAD_INFO_SSM_PARAM_PREFIX]}],
+            MaxResults=50,  # AWS allows up to 50 results per call
+            NextToken=next_token if next_token else "",
+        )
+
+        # Add parameters from this page
+        parameters.extend(response.get("Parameters", []))
+
+        # Check if more pages exist
+        next_token = response.get("NextToken")
+        if not next_token:
+            break
+
+    def get_parameter_value(name: str) -> str:
+        response = ssm_client.get_parameter(
+            Name=name,
+        )
+        return response["Parameter"]["Value"]
+
+    param_values: list[str] = []
+    for param in parameters:
+        param_name = param["Name"]
+        param_value = get_parameter_value(param_name)
+        param_values.append(param_value)
+
+    workloads_info = [AwsLogicalWorkload.model_validate_json(param) for param in param_values]
+
+    # for workload_info in workloads_info:
+    #     all_accounts = [*workload_info.prod_accounts, *workload_info.staging_accounts, *workload_info.dev_accounts]
+    #     for account in all_accounts:
+    #         deploy_role_arn = f"arn:aws:iam::{account.id}:role/InfraDeploy--{CENTRAL_INFRA_REPO_NAME}"
+
+    #         assume_role = ProviderAssumeRoleArgs(role_arn=central_infra_role_arn, session_name="pulumi")
+    #         central_infra_provider = Provider(
+    #             f"{central_infra_account_name}",
+    #             assume_role=assume_role,
+    #             allowed_account_ids=[central_infra_account.account.id],
+    #             region="us-east-1",
+    #             opts=ResourceOptions(
+    #                 parent=central_infra_account,
+    #             ),
+    #         )
