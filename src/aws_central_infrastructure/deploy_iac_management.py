95fa10a3128bafe6422725ad48ec9ba766caa254
Eli Fine <eli88fine@gmail.com>
2025-02-04 07:48:23 -0500

    Copier Init (#1)
    Instantiate the copier template and create the bucket policy

---
+++ src/aws_central_infrastructure/deploy_iac_management.py
@@ -0,0 +1,28 @@
+import logging
+from typing import Any
+
+from pulumi.automation import ConfigValue
+
+from .iac_management.program import pulumi_program
+from .pulumi_ephemeral_deploy.cli import run_cli
+
+logger = logging.getLogger(__name__)
+
+
+def generate_stack_config() -> dict[str, Any]:
+    """Generate the stack configuration."""
+    stack_config: dict[str, Any] = {}
+    stack_config["proj:pulumi_project_name"] = "iac-management"
+    github_repo_name = "aws-central-infrastructure"
+    stack_config["proj:github_repo_name"] = github_repo_name
+
+    stack_config["proj:git_repository_url"] = ConfigValue(value=f"https://github.com/ejfine/{github_repo_name}")
+    return stack_config
+
+
+def main() -> None:
+    run_cli(stack_config=generate_stack_config(), pulumi_program=pulumi_program)
+
+
+if __name__ == "__main__":
+    main()
