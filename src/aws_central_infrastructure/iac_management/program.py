342e5fe96fad3922790b6bec82c6ae3e6b7d65c3
Eli Fine <ejfine@gmail.com>
2025-02-04 20:57:30 +0000

    ssm params in subaccount

--- src/aws_central_infrastructure/iac_management/program.py
+++ src/aws_central_infrastructure/iac_management/program.py
@@ -1,21 +1,80 @@
 import logging
+from typing import TYPE_CHECKING
 
 import boto3
 from ephemeral_pulumi_deploy import get_aws_account_id
 from ephemeral_pulumi_deploy import get_config
 from ephemeral_pulumi_deploy import get_config_str
+from ephemeral_pulumi_deploy.utils import common_tags
+from pulumi import ComponentResource
+from pulumi import ResourceOptions
 from pulumi import export
+from pulumi.runtime import is_dry_run
 from pulumi_aws.iam import GetPolicyDocumentStatementArgs
 from pulumi_aws.iam import GetPolicyDocumentStatementConditionArgs
 from pulumi_aws.iam import GetPolicyDocumentStatementPrincipalArgs
 from pulumi_aws.iam import get_policy_document
 from pulumi_aws.organizations import get_organization
+from pulumi_aws_native import Provider
+from pulumi_aws_native import ProviderAssumeRoleArgs
 from pulumi_aws_native import s3
+from pulumi_aws_native import ssm
 
+from ..constants import CENTRAL_INFRA_REPO_NAME
 from .shared_lib import WORKLOAD_INFO_SSM_PARAM_PREFIX
 from .shared_lib import AwsLogicalWorkload
 
+if TYPE_CHECKING:
+    from mypy_boto3_ssm.type_defs import ParameterMetadataTypeDef
 logger = logging.getLogger(__name__)
+
+
+class AwsWorkloadPulumiBootstrap(ComponentResource):
+    def __init__(
+        self,
+        *,
+        workload: AwsLogicalWorkload,
+        organization_home_region: str,
+        central_state_bucket_name: str,
+        central_iac_kms_key_arn: str,
+    ):
+        super().__init__("labauto:AwsWorkloadPulumiBootstrap", workload.name, None)
+        all_accounts = [*workload.prod_accounts, *workload.staging_accounts, *workload.dev_accounts]
+        run_type = "Preview" if is_dry_run() else "Deploy"
+        for account in all_accounts:
+            role_arn = f"arn:aws:iam::{account.id}:role/Infra{run_type}--{CENTRAL_INFRA_REPO_NAME}"
+
+            assume_role = ProviderAssumeRoleArgs(role_arn=role_arn, session_name=f"pulumi-{run_type.lower()}")
+            provider = Provider(
+                f"central-infra-provider-for-{account.name}",
+                assume_role=assume_role,
+                allowed_account_ids=[account.id],
+                region=organization_home_region,
+                opts=ResourceOptions(
+                    parent=self,
+                    # TODO: figure out why ignore_changes isn't working
+                    ignore_changes=[
+                        "assumeRole.roleArn",
+                        "assumeRole.sessionName",
+                    ],  # ignore the ARN changes since during a preview we use the Preview Role, not the Deploy Role
+                ),
+            )
+            _ = ssm.Parameter(
+                f"central-infra-state-bucket-name-in-{account.name}",
+                type=ssm.ParameterType.STRING,
+                name="/org-managed/infra-state-bucket-name",
+                value=central_state_bucket_name,
+                opts=ResourceOptions(provider=provider, parent=self),
+                tags=common_tags(),
+            )
+            _ = ssm.Parameter(
+                "shared-kms-key-arn-in-{account.name}",
+                type=ssm.ParameterType.STRING,
+                name="/org-managed/infra-state-kms-key-arn",
+                value=central_iac_kms_key_arn,
+                opts=ResourceOptions(provider=provider, parent=self),
+                tags=common_tags(),
+            )
 
 
 def create_bucket_policy(bucket_name: str) -> str:
@@ -68,6 +127,7 @@ def pulumi_program() -> None:
     # Create Resources Here
     organization_home_region = "us-east-1"
     central_state_bucket_name = get_config_str("proj:backend_bucket_name")
+    kmy_key_arn = get_config_str("proj:kms_key_id")
     _ = s3.BucketPolicy(
         "bucket-policy",
         bucket=central_state_bucket_name,
@@ -76,7 +136,7 @@ def pulumi_program() -> None:
 
     ssm_client = boto3.client("ssm", region_name=organization_home_region)
 
-    parameters = []
+    parameters: list[ParameterMetadataTypeDef] = []
     next_token = None
 
     while True:
@@ -96,31 +156,26 @@ def pulumi_program() -> None:
             break
 
     def get_parameter_value(name: str) -> str:
-        response = ssm_client.get_parameter(
+        response = ssm_client.get_parameter(  # TODO: consider using get_parameters for just a single API call
             Name=name,
         )
-        return response["Parameter"]["Value"]
+        param_dict = response["Parameter"]
+        assert "Value" in param_dict, f"Value not found in parameter {param_dict}"
+        return param_dict["Value"]
 
     param_values: list[str] = []
     for param in parameters:
+        assert "Name" in param, f"Name not found in parameter {param}"
         param_name = param["Name"]
         param_value = get_parameter_value(param_name)
         param_values.append(param_value)
 
     workloads_info = [AwsLogicalWorkload.model_validate_json(param) for param in param_values]
 
-    # for workload_info in workloads_info:
-    #     all_accounts = [*workload_info.prod_accounts, *workload_info.staging_accounts, *workload_info.dev_accounts]
-    #     for account in all_accounts:
-    #         deploy_role_arn = f"arn:aws:iam::{account.id}:role/InfraDeploy--{CENTRAL_INFRA_REPO_NAME}"
-
-    #         assume_role = ProviderAssumeRoleArgs(role_arn=central_infra_role_arn, session_name="pulumi")
-    #         central_infra_provider = Provider(
-    #             f"{central_infra_account_name}",
-    #             assume_role=assume_role,
-    #             allowed_account_ids=[central_infra_account.account.id],
-    #             region="us-east-1",
-    #             opts=ResourceOptions(
-    #                 parent=central_infra_account,
-    #             ),
-    #         )
+    for workload_info in workloads_info:
+        _ = AwsWorkloadPulumiBootstrap(
+            workload=workload_info,
+            organization_home_region=organization_home_region,
+            central_state_bucket_name=central_state_bucket_name,
+            central_iac_kms_key_arn=kmy_key_arn,
+        )
