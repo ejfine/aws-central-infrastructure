4b55dbf14b4eba72dd580b87d7d14a22d4631457
Eli Fine <ejfine@gmail.com>
2025-02-16 01:52:09 +0000

    bucket policy names

--- src/aws_central_infrastructure/artifact_stores/lib/program.py
+++ src/aws_central_infrastructure/artifact_stores/lib/program.py
@@ -2,6 +2,7 @@ import logging
 
 from pulumi import export
 
+from .ssm_buckets import DistributorPackagesBucket
 from .ssm_buckets import ManualArtifactsBucket
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,8 @@ def pulumi_program() -> None:
     """Execute creating the stack."""
     # Create Resources Here
     manual_artifacts_bucket = ManualArtifactsBucket()
+    _ = DistributorPackagesBucket()
+
     export(
         "manual-artifacts-bucket-name", manual_artifacts_bucket.bucket.bucket_name
     )  # referenced by the Identity Center stack
--- src/aws_central_infrastructure/artifact_stores/lib/ssm_buckets.py
+++ src/aws_central_infrastructure/artifact_stores/lib/ssm_buckets.py
@@ -14,32 +14,34 @@ from pulumi_aws_native import s3
 logger = logging.getLogger(__name__)
 
 
+def create_worm_bucket(*, resource_name: str, parent: ComponentResource) -> s3.Bucket:
+    return s3.Bucket(
+        append_resource_suffix(resource_name),
+        versioning_configuration=s3.BucketVersioningConfigurationArgs(
+            status=s3.BucketVersioningConfigurationStatus.ENABLED
+        ),
+        object_lock_enabled=True,
+        object_lock_configuration=s3.BucketObjectLockConfigurationArgs(
+            object_lock_enabled="Enabled",
+            rule=s3.BucketObjectLockRuleArgs(
+                default_retention=s3.BucketDefaultRetentionArgs(mode=s3.BucketDefaultRetentionMode.GOVERNANCE, years=10)
+            ),
+        ),
+        opts=ResourceOptions(parent=parent),
+        tags=common_tags_native(),
+    )
+
+
 class ManualArtifactsBucket(ComponentResource):
     def __init__(
         self,
     ):
         super().__init__("labauto:ManualArtifactsBucket", append_resource_suffix(), None)
         # These artifacts are deployed to machines and devices. It's too much of a security risk to let people overwrite them, so setting up WORM.
-        self.bucket = s3.Bucket(
-            append_resource_suffix("manual-artifacts"),
-            versioning_configuration=s3.BucketVersioningConfigurationArgs(
-                status=s3.BucketVersioningConfigurationStatus.ENABLED
-            ),
-            object_lock_enabled=True,
-            object_lock_configuration=s3.BucketObjectLockConfigurationArgs(
-                object_lock_enabled="Enabled",
-                rule=s3.BucketObjectLockRuleArgs(
-                    default_retention=s3.BucketDefaultRetentionArgs(
-                        mode=s3.BucketDefaultRetentionMode.GOVERNANCE, years=10
-                    )
-                ),
-            ),
-            opts=ResourceOptions(parent=self),
-            tags=common_tags_native(),
-        )
+        self.bucket = create_worm_bucket(resource_name="manual-artifacts", parent=self)
         org_id = get_organization().id
         _ = s3.BucketPolicy(
-            "bucket-policy",
+            append_resource_suffix("manual-artifacts"),
             bucket=self.bucket.bucket_name,  # type: ignore[reportArgumentType] # pyright somehow thinks a bucket name can be Output[None], which doesn't seem possible
             policy_document=get_policy_document(
                 statements=[
@@ -48,7 +50,7 @@ class ManualArtifactsBucket(ComponentResource):
                         actions=["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
                         principals=[
                             GetPolicyDocumentStatementPrincipalArgs(
-                                type="*",
+                                type="*",  # TODO: consider locking this down to just people for PutObject
                                 identifiers=[
                                     "*"
                                 ],  # Anyone can do anything with this bucket if they themselves have been granted permission. WORM model keeps files secure.
@@ -57,12 +59,77 @@ class ManualArtifactsBucket(ComponentResource):
                         resources=["*"],
                         conditions=[
                             GetPolicyDocumentStatementConditionArgs(
+                                values=[org_id],
                                 test="StringEquals",
                                 variable="aws:PrincipalOrgID",
-                                values=[org_id],  # Limit to the AWS Organization
                             ),
                         ],
                     ),
                 ]
             ).json,
+        )
+
+
+class DistributorPackagesBucket(ComponentResource):
+    def __init__(
+        self,
+    ):
+        super().__init__("labauto:SsmDistributorPackagesBucket", append_resource_suffix(), None)
+        # These artifacts are deployed to machines and devices. It's too much of a security risk to let people overwrite them, so setting up WORM.
+        self.bucket = create_worm_bucket(resource_name="ssm-distributor-packages", parent=self)
+        org_id = get_organization().id
+        _ = s3.BucketPolicy(
+            append_resource_suffix("distributor-packages"),
+            bucket=self.bucket.bucket_name,  # type: ignore[reportArgumentType] # pyright somehow thinks a bucket name can be Output[None], which doesn't seem possible
+            policy_document=self.bucket.bucket_name.apply(
+                lambda bucket_name: get_policy_document(
+                    statements=[
+                        GetPolicyDocumentStatementArgs(
+                            effect="Allow",
+                            actions=["s3:PutObject", "s3:GetObject"],
+                            principals=[
+                                GetPolicyDocumentStatementPrincipalArgs(
+                                    type="AWS",
+                                    identifiers=[
+                                        "*:role/InfraDeploy*",
+                                        "*:role/InfraPreview*",
+                                    ],
+                                )
+                            ],
+                            conditions=[
+                                GetPolicyDocumentStatementConditionArgs(
+                                    variable="aws:PrincipalOrgID",
+                                    test="StringEquals",
+                                    values=[org_id],
+                                ),
+                            ],
+                            resources=[f"arn:aws:s3:::{bucket_name}/${{aws:PrincipalAccount}}/*"],
+                        ),
+                        GetPolicyDocumentStatementArgs(
+                            effect="Allow",
+                            actions=["s3:ListBucket"],
+                            principals=[
+                                GetPolicyDocumentStatementPrincipalArgs(
+                                    type="AWS",
+                                    identifiers=[
+                                        "*:role/InfraDeploy*",
+                                        "*:role/InfraPreview*",
+                                    ],
+                                )
+                            ],
+                            resources=[f"arn:aws:s3:::{bucket_name}"],
+                            conditions=[
+                                GetPolicyDocumentStatementConditionArgs(
+                                    variable="aws:PrincipalOrgID",
+                                    test="StringEquals",
+                                    values=[org_id],
+                                ),
+                                GetPolicyDocumentStatementConditionArgs(
+                                    test="StringLike", variable="s3:prefix", values=["${aws:PrincipalAccount}/*"]
+                                ),
+                            ],
+                        ),
+                    ]
+                ).json
+            ),
         )
