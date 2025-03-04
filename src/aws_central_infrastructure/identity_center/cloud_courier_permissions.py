40018a9020377fd078521b134cc912e9f3e83c16
Eli Fine <ejfine@gmail.com>
2025-03-04 22:34:39 +0000

    add comment

--- src/aws_central_infrastructure/identity_center/cloud_courier_permissions.py
+++ src/aws_central_infrastructure/identity_center/cloud_courier_permissions.py
@@ -5,6 +5,7 @@ from .lib.cloud_courier_permissions import create_cloud_courier_permissions
 
 
 def configure_cloud_courier_permissions(*, workload_info: AwsLogicalWorkload) -> None:
+    _ = all_created_users  # temporary line of code to avoid unused import error. This can be deleted as soon as actual users are granted Cloud Courier Permissions below
     create_cloud_courier_permissions(
         workload_info=workload_info,
         end_users=[all_created_users["eli.fine"]],
