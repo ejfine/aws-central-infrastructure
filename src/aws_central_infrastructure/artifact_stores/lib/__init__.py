a01612708a9191b059912ace5b5c0fff4183541b
Eli Fine <eli88fine@gmail.com>
2025-04-07 12:11:50 +0000

    copier update

--- src/aws_central_infrastructure/artifact_stores/lib/__init__.py
+++ src/aws_central_infrastructure/artifact_stores/lib/__init__.py
@@ -1,5 +1,4 @@
 from .code_artifact import RepoPackageClaims
-from .image_builder import USER_ACCESS_TAG_DELIMITER
 from .image_builder import ImageBuilderConfig
 from .image_builder import ImageShareConfig
 from .image_builder import NewImageConfig
--- src/aws_central_infrastructure/identity_center/lib/permissions.py
+++ src/aws_central_infrastructure/identity_center/lib/permissions.py
@@ -1,87 +1,21 @@
+import logging
 from collections.abc import Callable
 from typing import Any
 from typing import override
 
-from ephemeral_pulumi_deploy import common_tags
-from lab_auto_pulumi import AwsAccountInfo
 from lab_auto_pulumi import AwsLogicalWorkload
-from pulumi import ComponentResource
-from pulumi import ResourceOptions
-from pulumi_aws import identitystore as identitystore_classic
-from pulumi_aws import ssoadmin
+from lab_auto_pulumi import AwsSsoPermissionSet
+from lab_auto_pulumi import AwsSsoPermissionSetAccountAssignments
+from lab_auto_pulumi import UserInfo
 from pulumi_aws.iam import GetPolicyDocumentStatementArgs
 from pulumi_aws.iam import GetPolicyDocumentStatementConditionArgs
 from pulumi_aws.iam import get_policy_document
 from pydantic import BaseModel
 from pydantic import Field
 
-from .lib import ORG_INFO
-from .lib import UserAttributes
-from .lib import UserInfo
-from .lib import Username
 from .lib import create_read_state_inline_policy
 
-_all_users: dict[Username, UserAttributes] = {}
-
-
-def lookup_user_id(username: Username) -> str:
-    """Convert a username name into an AWS SSO User ID."""
-    return identitystore_classic.get_user(
-        alternate_identifier=identitystore_classic.GetUserAlternateIdentifierArgs(
-            unique_attribute=identitystore_classic.GetUserAlternateIdentifierUniqueAttributeArgs(
-                attribute_path="UserName", attribute_value=username
-            )
-        ),
-        identity_store_id=ORG_INFO.identity_store_id,
-    ).user_id
-
-
-class AwsSsoPermissionSet(ComponentResource):
-    def __init__(
-        self,
-        *,
-        name: str,
-        description: str,
-        managed_policies: list[str] | None = None,
-        inline_policy: str | None = None,
-        relay_state: str | None = None,
-    ):
-        super().__init__("labauto:AwsSsoPermissionSet", name, None)
-        if managed_policies is None:
-            managed_policies = []
-        self.name = name
-        permission_set = ssoadmin.PermissionSet(
-            name,
-            instance_arn=ORG_INFO.sso_instance_arn,
-            name=name,
-            description=description,
-            session_duration="PT12H",
-            opts=ResourceOptions(parent=self),
-            relay_state=relay_state,
-            tags=common_tags(),
-        )
-        self.permission_set_arn = permission_set.arn
-        for policy_name in managed_policies:
-            _ = ssoadmin.ManagedPolicyAttachment(
-                f"{name}-{policy_name}",
-                instance_arn=ORG_INFO.sso_instance_arn,
-                managed_policy_arn=f"arn:aws:iam::aws:policy/{policy_name}",
-                permission_set_arn=self.permission_set_arn,
-                opts=ResourceOptions(parent=self),
-            )
-        if inline_policy is not None:
-            _ = ssoadmin.PermissionSetInlinePolicy(
-                f"{name}-inline-policy",
-                instance_arn=ORG_INFO.sso_instance_arn,
-                permission_set_arn=self.permission_set_arn,
-                inline_policy=inline_policy,
-                opts=ResourceOptions(parent=self),
-            )
-        self.register_outputs(
-            {
-                "permission_set_arn": self.permission_set_arn,
-            }
-        )
+logger = logging.getLogger(__name__)
 
 
 class AwsSsoPermissionSetContainer(BaseModel):
@@ -292,6 +226,15 @@ EC2_SSO_PER_SET_CONTAINER = AwsSsoPermissionSetContainer(  # based on https://aw
                 ],
                 resources=["*"],
             ),
+            GetPolicyDocumentStatementArgs(
+                sid="GlobalS3",
+                effect="Allow",
+                actions=[
+                    "s3:ListAllMyBuckets",
+                    "s3:GetBucketLocation",
+                ],
+                resources=["*"],
+            ),
         ]
     ).json,
 )
@@ -302,50 +245,6 @@ ALL_PERM_SET_CONTAINERS = (
     VIEW_ONLY_PERM_SET_CONTAINER,
     EC2_SSO_PER_SET_CONTAINER,
 )
-
-
-def _create_unique_userinfo_list(users: list[UserInfo]) -> list[UserInfo]:
-    unique_user_infos: dict[Username, UserInfo] = {}
-    for user_info in users:
-        if user_info.username not in unique_user_infos:
-            unique_user_infos[user_info.username] = user_info
-            continue
-        info_in_dict = unique_user_infos[user_info.username]
-        if user_info == info_in_dict:
-            continue
-        raise ValueError(f"Duplicate user info for {user_info!r} and {info_in_dict!r}")  # noqa: TRY003 # not worth creating a custom exception until we test this # TODO: unit test this
-    return list(unique_user_infos.values())
-
-
-class AwsSsoPermissionSetAccountAssignments(ComponentResource):
-    def __init__(
-        self,
-        *,
-        account_info: AwsAccountInfo,
-        permission_set: AwsSsoPermissionSet,
-        users: list[UserInfo] | None = None,
-    ):
-        if users is None:
-            users = []
-        resource_name = f"{permission_set.name}-{account_info.name}"
-        super().__init__(
-            "labauto:AwsSsoPermissionSetAccountAssignments",
-            resource_name,
-            None,
-        )
-        user_infos = _create_unique_userinfo_list(users)
-
-        for user_info in user_infos:
-            _ = ssoadmin.AccountAssignment(
-                f"{resource_name}-{user_info.username}",
-                instance_arn=ORG_INFO.sso_instance_arn,
-                permission_set_arn=permission_set.permission_set_arn,
-                principal_id=lookup_user_id(user_info.username),
-                principal_type="USER",
-                target_id=account_info.id,
-                target_type="AWS_ACCOUNT",
-                opts=ResourceOptions(parent=self),
-            )
 
 
 class DefaultWorkloadPermissionAssignments(BaseModel):
