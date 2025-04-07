73007c25bd97b868b877d8f51506ac9205427f76
Eli Fine <eli88fine@gmail.com>
2025-04-07 12:24:21 +0000

    oidc

--- src/aws_central_infrastructure/iac_management/github_oidc.py
+++ src/aws_central_infrastructure/iac_management/github_oidc.py
@@ -1,11 +1,20 @@
 from lab_auto_pulumi import AwsLogicalWorkload
 from lab_auto_pulumi import WorkloadName
 
+from .lib import CENTRAL_INFRA_GITHUB_ORG_NAME
 from .lib import GithubOidcConfig
+from .lib import create_oidc_for_standard_workload
 
 
 def generate_oidc(
     *, workloads_info: dict[WorkloadName, AwsLogicalWorkload], all_oidc: dict[WorkloadName, list[GithubOidcConfig]]
 ) -> None:
     # create OIDC here
-    pass
+    workload_name = "elifine-com"
+    all_oidc[workload_name].extend(
+        create_oidc_for_standard_workload(
+            workload_info=workloads_info[workload_name],
+            repo_org=CENTRAL_INFRA_GITHUB_ORG_NAME,
+            repo_name="elifine-com",
+        )
+    )
