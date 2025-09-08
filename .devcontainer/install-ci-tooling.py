2144cd8c1a2691e6f3b6eb784adf3b249153584f
Eli Fine <eli88fine@gmail.com>
2025-09-08 09:56:42 +0000

    copier

--- .devcontainer/install-ci-tooling.py
+++ .devcontainer/install-ci-tooling.py
@@ -7,10 +7,10 @@ import sys
 import tempfile
 from pathlib import Path
 
-UV_VERSION = "0.8.4"
-PNPM_VERSION = "10.14.0"
-COPIER_VERSION = "9.9.1"
-COPIER_TEMPLATE_EXTENSIONS_VERSION = "0.3.2"
+UV_VERSION = "0.8.15"
+PNPM_VERSION = "10.15.1"
+COPIER_VERSION = "9.10.1"
+COPIER_TEMPLATE_EXTENSIONS_VERSION = "0.3.3"
 PRE_COMMIT_VERSION = "4.3.0"
 GITHUB_WINDOWS_RUNNER_BIN_PATH = r"C:\Users\runneradmin\.local\bin"
 INSTALL_SSM_PLUGIN_BY_DEFAULT = False
@@ -153,10 +153,7 @@ def main():
                     ["sudo", "dpkg", "-i", str(local_package_path)],
                     check=True,
                 )
-            _ = subprocess.run(
-                ["echo", "SSM Plugin Manager Version:"],
-                check=True,
-            )
+            print("SSM Plugin Manager Version: ")
             _ = subprocess.run(
                 ["session-manager-plugin", "--version"],
                 check=True,
