df2cb3110de98f1023b99bd9f60cc6fe00f924b1
Eli Fine <eli88fine@gmail.com>
2025-12-24 01:57:07 +0000

    version

--- src/aws_central_infrastructure/identity_center/lib/permissions.py
+++ src/aws_central_infrastructure/identity_center/lib/permissions.py
@@ -90,7 +90,6 @@ def create_manual_artifacts_bucket_access_inline_policy() -> str:
                     "s3:GetObjectVersion",
                     "s3:PutObject",
                     "s3:DeleteObject",
-                    "s3:DeleteObjectVersion",
                 ],
                 resources=["arn:aws:s3:::*/*"],
                 conditions=[
