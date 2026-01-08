2bb9ac7b7f8d14b510c1ffe26b89cb48ed56999a
Eli Fine <eli88fine@gmail.com>
2026-01-08 20:46:07 +0000

    pulumi

--- src/aws_central_infrastructure/github_repos/teams.py
+++ src/aws_central_infrastructure/github_repos/teams.py
@@ -10,8 +10,34 @@ def define_team_configs(
 
     example: `configs.append(GithubTeamConfig(name="Manhattan Project Team", description="Working on something big"))`
     """
-    _ = configs  # this line can be removed once the first team is appended on to configs, it just temporarily helps linting when the template is instantiated
     _ = dev_sec_ops_team_config  # this line can be removed once any adjustments have been made to the DevSecOps team config
     org_members = GithubOrgMembers(org_admins=[ROOT_GITHUB_ADMIN_USERNAME])
-    org_members.everyone.extend([])
+    org_members.everyone.extend(["zendern"])
+    configs.append(
+        GithubTeamConfig(
+            name="Copier Templates",
+            description="Copier Templates Team",
+            members=["ejfine", "zendern"],
+            repo_permissions={
+                "copier-base-template": "push",
+                "copier-aws-organization": "push",
+                "copier-aws-central-infrastructure": "push",
+                "copier-python-package-template": "push",
+                "copier-pulumi-project": "push",
+                "copier-nuxt-python-intranet-app": "push",
+                "copier-nuxt-static-aws": "push",
+            },
+        )
+    )
+    configs.append(
+        GithubTeamConfig(
+            name="Pulumi Libraries",
+            description="Copier Templates Team",
+            members=["ejfine", "zendern"],
+            repo_permissions={
+                "ephemeral-pulumi-deploy": "push",
+                "lab-auto-pulumi": "push",
+            },
+        )
+    )
     return org_members
