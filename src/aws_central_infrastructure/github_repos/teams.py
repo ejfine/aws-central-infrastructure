46697c44d693e50a4e4b860eda32591f40f904e4
Eli Fine <eli88fine@gmail.com>
2026-03-13 06:12:59 -0700

    Add Ian to repos (#97)
    New contributor
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Administrative Updates**
    * Added new team member to the organization with access to multiple
    project teams.
    * Extended repository permissions to provide additional access to
    package template resources.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/aws_central_infrastructure/github_repos/teams.py
+++ src/aws_central_infrastructure/github_repos/teams.py
@@ -12,12 +12,12 @@ def define_team_configs(
     """
     _ = dev_sec_ops_team_config  # this line can be removed once any adjustments have been made to the DevSecOps team config
     org_members = GithubOrgMembers(org_admins=[ROOT_GITHUB_ADMIN_USERNAME])
-    org_members.everyone.extend(["zendern"])
+    org_members.everyone.extend(["zendern", "idonaldson"])
     configs.append(
         GithubTeamConfig(
             name="Copier Templates",
             description="Copier Templates Team",
-            members=["ejfine", "zendern"],
+            members=["ejfine", "zendern", "idonaldson"],
             repo_permissions={
                 "copier-base-template": "push",
                 "copier-aws-organization": "push",
@@ -26,6 +26,7 @@ def define_team_configs(
                 "copier-pulumi-project": "push",
                 "copier-nuxt-python-intranet-app": "push",
                 "copier-nuxt-static-aws": "push",
+                "copier-vue-package-template": "push",
             },
         )
     )
@@ -33,7 +34,7 @@ def define_team_configs(
         GithubTeamConfig(
             name="Pulumi Libraries",
             description="Pulumi Libraries Team",
-            members=["ejfine", "zendern"],
+            members=["ejfine", "zendern", "idonaldson"],
             repo_permissions={
                 "ephemeral-pulumi-deploy": "push",
                 "lab-auto-pulumi": "push",
@@ -44,7 +45,7 @@ def define_team_configs(
         GithubTeamConfig(
             name="LaTeX",
             description="LaTeX Team",
-            members=["ejfine", "zendern"],
+            members=["ejfine", "zendern", "idonaldson"],
             repo_permissions={
                 "latex-presentations": "push",
             },
