031fb8bed9d05ba7a0241b928889c34a9010fa0b
Eli Fine <eli88fine@gmail.com>
2026-07-17 21:52:56 +0000

    temp remove teams

--- src/aws_central_infrastructure/github_repos/teams.py
+++ src/aws_central_infrastructure/github_repos/teams.py
@@ -11,7 +11,9 @@ from .lib import GithubTeamConfig
 
 
 def define_team_configs(
-    *, configs: list[GithubTeamConfig], dev_sec_ops_team_config: GithubTeamConfig
+    *,
+    configs: list[GithubTeamConfig],  # noqa: ARG001 # temp
+    dev_sec_ops_team_config: GithubTeamConfig,
 ) -> GithubOrgMembers:
     """Create the configurations for the repositories.
 
@@ -20,42 +22,5 @@ def define_team_configs(
     _ = dev_sec_ops_team_config  # this line can be removed once any adjustments have been made to the DevSecOps team config
     org_members = GithubOrgMembers(org_admins=[ROOT_GITHUB_ADMIN_USERNAME])
     org_members.everyone.extend(["zendern", "idonaldson"])
-    configs.append(
-        GithubTeamConfig(
-            name="Copier Templates",
-            description="Copier Templates Team",
-            members=["ejfine", "zendern", "idonaldson"],
-            repo_permissions={
-                "copier-base-template": "push",
-                "copier-aws-organization": "push",
-                "copier-aws-central-infrastructure": "push",
-                "copier-python-package-template": "push",
-                "copier-pulumi-project": "push",
-                "copier-nuxt-python-intranet-app": "push",
-                "copier-nuxt-static-aws": "push",
-                "copier-vue-package-template": "push",
-            },
-        )
-    )
-    configs.append(
-        GithubTeamConfig(
-            name="Pulumi Libraries",
-            description="Pulumi Libraries Team",
-            members=["ejfine", "zendern", "idonaldson"],
-            repo_permissions={
-                "ephemeral-pulumi-deploy": "push",
-                "lab-auto-pulumi": "push",
-            },
-        )
-    )
-    configs.append(
-        GithubTeamConfig(
-            name="LaTeX",
-            description="LaTeX Team",
-            members=["ejfine", "zendern", "idonaldson"],
-            repo_permissions={
-                "latex-presentations": "push",
-            },
-        )
-    )
+
     return org_members
